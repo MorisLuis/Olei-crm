91e1a9ed69313296b6a9e9b2522b54b030d60c9c
“MorisLuis” <moradoluisenrique@gmail.com>
2024-11-26 10:14:12 -0600

    S / Calendar UI

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,21 +1,37 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
 import MyCalendar from './Calendar';
-import Header from '@/components/navigation/header';
+import Header, { ActionsInterface } from '@/components/navigation/header';
 import { useRouter } from 'next/navigation';
+import FormMeeting from '../bitacora/FormMeeting';
 
 export default function Calendar() {
 
     const { push } = useRouter()
+    const [openModalCreateMeeting, setOpenModalCreateMeeting] = useState(false)
 
+    const clientActions: ActionsInterface[] = [
+        {
+            id: 1,
+            text: 'Nueva Reunión',
+            onclick: () => setOpenModalCreateMeeting(true),
+            color: 'yellow'
+        }
+    ]
 
     return (
         <div>
-            <Header title='Calendario' />
-            <MyCalendar 
+            <Header title='Calendario' actions={clientActions} />
+
+            <MyCalendar
                 onClickEvent={() => push(`calendar/event/1`)}
                 onClickDay={() => console.log(true)}
+            />
+
+            <FormMeeting
+                visible={openModalCreateMeeting}
+                onClose={() => setOpenModalCreateMeeting(false)}
             />
 
         </div>
