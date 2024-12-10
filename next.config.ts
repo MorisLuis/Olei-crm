c3730c90a41dd7ca70ad228f8b441cc23bb64b8b
“MorisLuis” <moradoluisenrique@gmail.com>
2024-12-10 10:43:38 -0600

    S / Redirection

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,21 @@ const nextConfig: NextConfig = {
     silenceDeprecations: ["legacy-js-api"],
     // Opciones específicas para Sass
     // Si "silenceDeprecations" es algo personalizado, retíralo si no está documentado.
-  }
+  },
+  redirects: async () => [
+    {
+      source: '/',
+      destination: '/login',
+      permanent: true, // true para redirección 301 permanente, false para 302 temporal
+    },
+    { source: '/bitacora', destination: '/dashboard/bitacora', permanent: true},
+    { source: '/calendar', destination: '/dashboard/calendar', permanent: true},
+    { source: '/clients', destination: '/dashboard/clients', permanent: true},
+    { source: '/cobranza', destination: '/dashboard/cobranza', permanent: true},
+    { source: '/sells', destination: '/dashboard/sells', permanent: true},
+    { source: '/settings', destination: '/dashboard/settings', permanent: true}
+  ],
+
 };
 
 export default nextConfig;
