c9be6f76f5eff0e885edd60f32cafef6c727b106
“MorisLuis” <moradoluisenrique@gmail.com>
2025-08-30 18:24:24 -0600

    update(dateValidation) - update date handling, add total sales and logs, and clean up unused validations.

--- utils/validators/FormMeetingValidation.ts
+++ utils/validators/FormMeetingValidation.ts
@@ -1,22 +1,4 @@
 
-export const dateValidation = (date: Date | string): boolean => {
-
-  let dateString: string;
-
-  if (date instanceof Date) {
-    dateString = date.toISOString().split('T')[0]; // Convertir Date a 'YYYY-MM-DD'
-  } else if (typeof date === 'string') {
-    dateString = transformDate(date);
-  } else {
-    return false; // No es un Date ni un string válido
-  }
-
-  // Expresión regular para validar el formato 'YYYY-MM-DD'
-  const regex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
-
-  return regex.test(dateString);
-};
-
 export const hourValidation = (hour: string): boolean => {
   const regex = /^(?:[01]\d|2[0-3]):[0-5]\d$/;
   return regex.test(hour);
@@ -25,17 +7,4 @@ export const hourValidation = (hour: string): boolean => {
 export const emailValidation = (email: string): boolean => {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
-};
-
-/* UTILS */
-const transformDate = (date: string): string => {
-  let dateString;
-  if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(date)) {
-    // Si tiene formato 'YYYY-MM-DDTHH:mm:ss.sssZ', lo convertimos
-    dateString = date.split('T')[0];
-  } else {
-    dateString = date; // Mantenerlo como está si ya es 'YYYY-MM-DD'
-  }
-
-  return dateString
 };
---
+++ utils/validators/dateValidation.ts
@@ -0,0 +1,42 @@
+/**
+ * 
+ * Validates if the given date is in 'YYYY-MM-DD' format.
+ * @params date could be a Date object or a string in 'YYYY-MM-DD' or 'YYYY-MM-DDTHH:mm:ss.sssZ' format   
+ * @returns boolean
+ * @throws Error if the date is not valid
+ * @example dateValidation(new Date()) // true
+ */
+
+export const dateValidation = (date: Date | string): boolean => {
+
+    console.log("date instanceof Date", date instanceof Date)
+    console.log({ date })
+    let dateString: string;
+
+
+    if (date instanceof Date) {
+        // Convertir Date a 'YYYY-MM-DD'
+        dateString = date.toISOString().split('T')[0];
+    } else if (typeof date === 'string') {
+        dateString = transformDate(date);
+    } else {
+        return false;
+    }
+
+    // Regular expression to validate 'YYYY-MM-DD' format
+    const regex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
+
+    return regex.test(dateString);
+};
+
+const transformDate = (date: string): string => {
+    let dateString;
+    if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(date)) {
+        // If the date is in 'YYYY-MM-DDTHH:mm:ss.sssZ' format, convert it to 'YYYY-MM-DD'
+        dateString = date.split('T')[0];
+    } else {
+        dateString = date;
+    }
+
+    return dateString
+};
